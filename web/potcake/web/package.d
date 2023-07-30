76e995f9accaafd75c827a91a3a662a331a2b926
Kyle Ingraham <kyle@kyleingraham.com>
2023-07-30 09:16:01 -0400

    Add Environment Variable Convenience Method
        Add a method for fetching an environment variable and optionally
        converting it to a given type.
    
        Additional changes:
        
        Add additional test for isValidHandler to cover the type of handler
        that vibe.d generates in staticTemplate.

--- web/potcake/web/package.d
+++ web/potcake/web/package.d
@@ -1,3 +1,4 @@
 module potcake.web;
 
 public import potcake.web.app;
+public import potcake.web.utils;
---
+++ web/potcake/web/utils.d
@@ -0,0 +1,88 @@
+module potcake.web.utils;
+@safe:
+
+/**
+ * Get an environment variable and optionally convert it to a given type.
+
+   When given `bool` as the conversion type the following applies:
+     - True values are "y", "yes", "t", "true", "on", and "1"
+     - False values are "n", "no", "f", "false", "off", and "0"
+     - Environment variable comparison is case-insensitive
+     - ConvException is thrown when conversion fails
+
+   Parameter types are set to match those of std.process.environment.get.
+ */
+T getEnvironmentVariable(T = string)(scope const(char)[] name, string defaultValue = null)
+{
+    import std.conv : ConvException;
+    import std.process : environment;
+
+    string value = environment.get(name, defaultValue);
+
+    static if (is(T : bool))
+    {
+        import std.algorithm.comparison : equal;
+        import std.algorithm.searching : any;
+        import std.uni : toLower;
+
+        string lowerValue = value.toLower;
+
+        auto predicate = (string a) => lowerValue.equal(a);
+
+        if (any!(predicate)(["y", "yes", "t", "true", "on", "1"]))
+            return true;
+        else if (any!(predicate)(["n", "no", "f", "false", "off", "0"]))
+            return false;
+        else
+            throw new ConvException("'" ~ value ~ "' could not be converted to a bool");
+
+    } else
+    {
+        import std.conv : to;
+
+        return to!T(value);
+    }
+}
+
+///
+unittest
+{
+    import std.conv : ConvException;
+    import std.process : environment;
+    import unit_threaded.assertions : shouldEqual, shouldThrowWithMessage;
+
+    string potcakeTestVariable = "POTCAKE_TEST_VARIABLE";
+
+    scope(exit) environment[potcakeTestVariable] = null;
+
+    environment[potcakeTestVariable] = "42";
+    auto testInt = getEnvironmentVariable!int(potcakeTestVariable);
+    testInt.shouldEqual(42);
+
+    environment[potcakeTestVariable] = "production";
+    auto testString = getEnvironmentVariable(potcakeTestVariable);
+    testString.shouldEqual("production");
+
+    bool testBool;
+
+    foreach (truthyValue; ["y", "yes", "t", "true", "on", "1", "True"])
+    {
+        environment[potcakeTestVariable] = truthyValue;
+        testBool = getEnvironmentVariable!bool(potcakeTestVariable);
+        testBool.shouldEqual(true);
+    }
+
+    foreach (falseyValue; ["n", "no", "f", "false", "off", "0", "False"])
+    {
+        environment[potcakeTestVariable] = falseyValue;
+        testBool = getEnvironmentVariable!bool(potcakeTestVariable);
+        testBool.shouldEqual(false);
+    }
+
+    environment[potcakeTestVariable] = "Potcake";
+    getEnvironmentVariable!bool(
+        potcakeTestVariable
+    ).shouldThrowWithMessage!ConvException(
+        "'Potcake' could not be converted to a bool"
+    );
+}
