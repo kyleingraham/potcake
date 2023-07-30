ef9b18bb96559c17e680a41e2ba04677bc2e9f1f
Kyle Ingraham <kyle@kyleingraham.com>
2023-07-30 11:10:34 -0400

    Rename Environment Variable Fetcher
        Rename Potcake's environment variable fetching function to be a bit
        more compact.

--- web/potcake/web/utils.d
+++ web/potcake/web/utils.d
@@ -5,14 +5,14 @@ module potcake.web.utils;
  * Get an environment variable and optionally convert it to a given type.
 
    When given `bool` as the conversion type the following applies:
-     - True values are "y", "yes", "t", "true", "on", and "1"
-     - False values are "n", "no", "f", "false", "off", and "0"
-     - Environment variable comparison is case-insensitive
-     - ConvException is thrown when conversion fails
+   - True values are "y", "yes", "t", "true", "on", and "1"
+   - False values are "n", "no", "f", "false", "off", and "0"
+   - Environment variable comparison is case-insensitive
+   - ConvException is thrown when conversion fails
 
-   Parameter types are set to match those of std.process.environment.get.
+   Parameter types are set to match those of `std.process.environment.get`.
  */
-T getEnvironmentVariable(T = string)(scope const(char)[] name, string defaultValue = null)
+T getEnvVar(T = string)(scope const(char)[] name, string defaultValue = null)
 {
     import std.conv : ConvException;
     import std.process : environment;
@@ -56,31 +56,35 @@ unittest
     scope(exit) environment[potcakeTestVariable] = null;
 
     environment[potcakeTestVariable] = "42";
-    auto testInt = getEnvironmentVariable!int(potcakeTestVariable);
+    auto testInt = getEnvVar!int(potcakeTestVariable);
     testInt.shouldEqual(42);
 
     environment[potcakeTestVariable] = "production";
-    auto testString = getEnvironmentVariable(potcakeTestVariable);
+    auto testString = getEnvVar(potcakeTestVariable);
     testString.shouldEqual("production");
+
+    environment[potcakeTestVariable] = null;
+    testString = getEnvVar(potcakeTestVariable, "development");
+    testString.shouldEqual("development");
 
     bool testBool;
 
     foreach (truthyValue; ["y", "yes", "t", "true", "on", "1", "True"])
     {
         environment[potcakeTestVariable] = truthyValue;
-        testBool = getEnvironmentVariable!bool(potcakeTestVariable);
+        testBool = getEnvVar!bool(potcakeTestVariable);
         testBool.shouldEqual(true);
     }
 
     foreach (falseyValue; ["n", "no", "f", "false", "off", "0", "False"])
     {
         environment[potcakeTestVariable] = falseyValue;
-        testBool = getEnvironmentVariable!bool(potcakeTestVariable);
+        testBool = getEnvVar!bool(potcakeTestVariable);
         testBool.shouldEqual(false);
     }
 
     environment[potcakeTestVariable] = "Potcake";
-    getEnvironmentVariable!bool(
+    getEnvVar!bool(
         potcakeTestVariable
     ).shouldThrowWithMessage!ConvException(
         "'Potcake' could not be converted to a bool"
